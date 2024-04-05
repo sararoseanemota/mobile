180f5ce59094888977f193564afa2829fca3774f
sararoseanesm@gmail.com <sararoseanemota>
2024-04-05 17:23:26 -0300

    feat: views index, register and layout.

---
+++ src/app/index.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { View, Image, Alert } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+import { colors } from "@/styles/colors";
+
+export default function Home() {
+  const [code, setCode] = useState("");
+
+  function handleAccessCredential() {
+    if (!code.trim()) {
+      return Alert.alert("Ingresso", "Informe o código do ingresso!");
+    }
+  }
+
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar style="light" />
+
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field
+            placeholder="Código do ingresso"
+            onChangeText={setCode}
+          />
+        </Input>
+
+        <Button title="Acessar credencial" onPress={handleAccessCredential} />
+
+        <Link
+          href="/register"
+          className="text-gray-100 text-base font-bold text-center mt-8"
+        >
+          Ainda não possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
+}
---
+++ src/app/register.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { View, Image, Alert } from "react-native";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
+import { Link, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+import { colors } from "@/styles/colors";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleRegister() {
+    if (!name.trim() || !email.trim()) {
+      return Alert.alert("Inscrição", "Preencha todos os campos!");
+    }
+
+    router.push("/ticket");
+  }
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar style="light" />
+
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <FontAwesome6
+            name="user-circle"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="Nome Completo" onChangeText={setName} />
+        </Input>
+        <Input>
+          <MaterialIcons
+            name="alternate-email"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field
+            placeholder="E-mail"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+          />
+        </Input>
+
+        <Button title="Realizar inscrição" onPress={handleRegister} />
+
+        <Link
+          href="/"
+          className="text-gray-100 text-base font-bold text-center mt-8"
+        >
+          Já possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
+}
