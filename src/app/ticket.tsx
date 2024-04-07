03271f0cd83b49b673a351b46ec397ac552ac9b8
sararoseanesm@gmail.com <sararoseanemota>
2024-04-07 12:50:45 -0300

    feat: create badgeStore and animated

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,26 +1,48 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 import {
   Text,
   View,
   Alert,
   Modal,
+  Share,
   StatusBar,
   ScrollView,
   TouchableOpacity,
-} from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
+} from 'react-native';
 
-import { Header } from "@/components/header";
-import { Credential } from "@/components/credential";
-import { colors } from "@/styles/colors";
-import { Button } from "@/components/button";
-import { QrCode } from "@/components/qrcode";
+import { FontAwesome } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+import { Redirect } from 'expo-router';
+
+import { MotiView } from 'moti/build';
+
+import { Header } from '@/components/header';
+import { QrCode } from '@/components/qrcode';
+import { Button } from '@/components/button';
+import { Credential } from '@/components/credential';
+
+import { colors } from '@/styles/colors';
+
+import { useBadgeStore } from '@/store/badge-store';
 
 export default function Ticket() {
-  const [image, setImage] = useState("");
   const [expandQrCode, setExpandQrCode] = useState(false);
+
+  const badgeStore = useBadgeStore();
+
+  async function handleShare() {
+    if (badgeStore.data?.checkInURL) {
+      await Share.share({
+        message: badgeStore.data.checkInURL,
+      });
+    }
+    try {
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Compartilhar', 'Não foi possível compartilhar!');
+    }
+  }
 
   async function handleSelectImage() {
     try {
@@ -30,14 +52,17 @@ export default function Ticket() {
         aspect: [4, 4],
       });
       if (result.assets) {
-        setImage(result.assets[0].uri);
+        badgeStore.updateAvatar(result.assets[0].uri);
       }
     } catch (error) {
       console.log(error);
-      Alert.alert("Foto", "Não foi possível selecionar a imagem.");
+      Alert.alert('Foto', 'Não foi possível selecionar a imagem.');
     }
   }
 
+  if (!badgeStore.data?.checkInURL) {
+    return <Redirect href="/" />;
+  }
   return (
     <View className="flex-1 bg-green-500">
       <StatusBar barStyle="light-content" />
@@ -49,29 +74,43 @@ export default function Ticket() {
         showsVerticalScrollIndicator={false}
       >
         <Credential
-          image={image}
+          data={badgeStore.data}
           onChangeAvatar={handleSelectImage}
           onExpandQrCode={() => setExpandQrCode(true)}
         />
 
-        <FontAwesome
-          name="angle-double-down"
-          size={24}
-          color={colors.gray[300]}
-          className="self-center my-6"
-        />
+        <MotiView
+          from={{ translateY: 0 }}
+          animate={{ translateY: 10 }}
+          transition={{
+            loop: true,
+            type: 'timing',
+            duration: 700,
+          }}
+        >
+          <FontAwesome
+            name="angle-double-down"
+            size={24}
+            color={colors.gray[300]}
+            className="self-center my-6"
+          />
+        </MotiView>
 
         <Text className="text-white font-bold text-2xl mt-4">
           Compartilhar Credencial
         </Text>
 
         <Text className="text-white font-regular text-base mt-1 mb-6">
-          Mostre ao mundo que você vai participar do Unite Summit!
+          Mostre ao mundo que você vai participar do evento{' '}
+          {badgeStore.data.eventTitle}!
         </Text>
 
-        <Button title="Compartilhar" />
+        <Button title="Compartilhar" onPress={handleShare} />
 
-        <TouchableOpacity activeOpacity={0.7}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => badgeStore.remove()}
+        >
           <Text className="text-base text-white font-bold text-center mt-10">
             Remover ingresso
           </Text>
